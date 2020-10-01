1ac3fe561dd1459cdab7e478502eb4acfd5cb266
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-01 18:43:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,11 @@ require 'pry'
 
 def start_game(player1, player2)
  p "Hello #{player1} & #{player2}"
-  binding.pry
 end
 
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
+  binding.pry
 end
 
